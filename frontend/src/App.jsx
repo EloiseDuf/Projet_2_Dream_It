52445085075247f9f3bcbdf91cab2bd30fc7fe74
HugoHtvl <hug.oo@hotmail.fr>
2023-06-14 16:48:01 +0200

    route settings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <p>coucou</p>
     </div>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,36 +1,12 @@
-import Counter from "../components/Counter";
-import logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
 
-export default function Home() {
+const Home = () => {
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Hello Vite + React !</p>
-
-      <Counter />
-
-      <p>
-        Edit <code>App.jsx</code> and save to test HMR updates.
-      </p>
-      <p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {" | "}
-        <a
-          className="App-link"
-          href="https://vitejs.dev/guide/features.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vite Docs
-        </a>
-      </p>
-    </header>
+    <>
+      <p>je suis dans le comp Home</p>
+      <Link to="/">Home</Link>
+    </>
   );
-}
+};
+
+export default Home;
