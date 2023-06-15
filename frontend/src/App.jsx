eea3270bf785b57e52764f1187cbc0703e0eb412
Jerome Desouches <jerome.desouches@gmail.com>
2023-06-15 16:53:47 +0200

    filters en place, manque barre de recherche + interractions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Filters from "./components/Filters";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Filters />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
