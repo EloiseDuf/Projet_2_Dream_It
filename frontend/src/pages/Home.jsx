d6db5388bb3f0128c98fee92f89f4266e696c61d
Nestadar <Nestadar@gmail.com>
2023-06-15 12:06:16 +0200

    SwitchButton component added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,22 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import SwitchButton from "../components/SwitchButton";
+import "./Home.scss";
 
 function Home() {
+  const [isOn, setIson] = useState(true);
+
+  const handleActive = (isOn) => {
+    setIson(!isOn);
+  };
+
   return (
     <>
       <p>je suis dans le comp Home</p>
       <Link to="/">Home</Link>
+      <div className={`Light ${isOn ? "Light" : "Dark"}`}>
+        <SwitchButton Active={handleActive} />
+      </div>
     </>
   );
 }
