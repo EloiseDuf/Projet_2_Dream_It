1ae3f631dc0d2ffa604a045edec3447540ab2adb
Jerome Desouches <jerome.desouches@gmail.com>
2023-06-22 10:23:56 +0200

    ajout sous menu dans bouton profil => ni agencé css ni fonctionnalités avancées

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,10 @@
 import React, { useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import MyContext from "./Context";
 import "./NavBar.scss";
+import logoDreamIt from "../assets/images/deam it LOGOLogo.png";
+import logoAccount from "../assets/images/Account.png";
+import cartHome from "../assets/images/CartHome.png";
 
 function NavBar() {
   const { user, setUser, users } = useContext(MyContext);
@@ -31,23 +34,50 @@ function NavBar() {
 
   return (
     <div className="NavBar">
-      <div className="homeRightDiv">
-        <img src="src/assets/images/deam it LOGOLogo.png" alt="Logo" />
+      <Link to="/" className="homeRightDiv">
+        <img src={logoDreamIt} alt="Logo" />
         <p>A la carte</p>
         <p>Préfabriqué</p>
-      </div>
+      </Link>
       <div className="homeLeftDiv">
         <button id="Account" type="button">
-          <img
-            src="src/assets/images/Account.png"
-            alt="Account"
-            onClick={handleClickConnerter}
-          />
+          <img src={logoAccount} alt="Account" onClick={handleClickConnerter} />
         </button>
         <button id="Cart" type="button">
-          <img src="src/assets/images/CartHome.png" alt="Cart" />
+          <img src={cartHome} alt="Cart" />
           <span className="item-count" />
         </button>
+        <div className="menuProfilNavbar">
+          {/* Ajouter 2 div
+        la première contient la photo et le pseudo de l'utilisateur et n'est affiché que si l'utilisateur est connecté
+        la 2eme est un bouton Se connecter / se déconnecter qui change en fonction de l'état de la variable "user"
+        si user === null ? Se connecter : Se déconnecter
+        user est déjà un usestate partagé avec toutes les pages (je vous expliquerai) */}
+
+          {/* Link à remplacer par des div avec une fonction onClick et un navigate à l'intérieur 
+        Si utilisateur connecté --> navigate direct
+        Si utilisateur pas connecté : redirection vers POPUP pour se connecter
+        utilisation d'un useState pour stocker le lien à suivre après s'être connecté dans la popup
+        Si on clique sur se déconnecter on redirige vers la page home */}
+          <Link to="/profil" className="linkProfilElementNavbar">
+            Mon profil
+          </Link>
+          <Link
+            to="/profil/commandesection"
+            className="linkProfilElementNavbar"
+          >
+            Mes commandes
+          </Link>
+          <Link to="/profil/favorissection" className="linkProfilElementNavbar">
+            Mes favoris
+          </Link>
+          <Link
+            to="/profil/demandeparticuliere"
+            className="linkProfilElementNavbar"
+          >
+            Demandes particulières
+          </Link>
+        </div>
       </div>
     </div>
   );
