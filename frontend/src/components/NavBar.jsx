40532993b0e079104f1d39d290e979b04e0299f0
Nestadar <Nestadar@gmail.com>
2023-06-27 18:08:41 +0200

    css in progress, items are nearly perferctly aligned

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import MyContext from "./Context";
 import "./NavBar.scss";
 import logoDreamIt from "../assets/images/deam it LOGOLogo.png";
@@ -11,8 +11,6 @@ function NavBar() {
   const { user, setUser } = useContext(MyContext);
   const [isDivVisible, setIsDivVisible] = useState(false);
   const [isPopupOpen, setIsPopupOpen] = useState(false);
-
-  const navigate = useNavigate();
 
   const handleOpenPopup = () => {
     setIsPopupOpen(true);
@@ -35,32 +33,72 @@ function NavBar() {
   const handleMouseLeave = () => {
     setIsDivVisible(false);
   };
+
   return (
-    <div className="NavBar">
-      <div className="homeRightDiv">
-        <Link to="/">
-          <img src={logoDreamIt} alt="Logo" />
+    <div className="toutesLesDivs">
+      <div className="NavBar">
+        <div className="homeRightDiv">
+          <Link to="/">
+            <img src={logoDreamIt} alt="Logo" />
+          </Link>
           <p>A la carte</p>
-        </Link>
-        <Link to="/bundle">Préfabriqué</Link>
-      </div>
-      <Popup
-        isOpen={isPopupOpen}
-        onClose={handleClosePopup}
-        // onSubmit={handleSubmitPopup}
-      />
-      <div className="homeLeftDiv">
-        <button id="Account" type="button">
-          <img src={logoAccount} alt="Account" onClick={handleClickNavMenu} />
-        </button>
-        <button id="Cart" type="button">
-          <img src={cartHome} alt="Cart" />
-          <span className="item-count" />
-        </button>
+          <Link to="/bundle">Préfabriqué</Link>
+        </div>
+        <Popup
+          isOpen={isPopupOpen}
+          onClose={handleClosePopup}
+          // onSubmit={handleSubmitPopup}
+        />
+        <div className="homeLeftDiv">
+          <div className="test">
+            <button id="Account" type="button">
+              <img
+                src={logoAccount}
+                alt="Account"
+                onClick={handleClickNavMenu}
+              />
+            </button>
+            <button id="Cart" type="button">
+              <img src={cartHome} alt="Cart" />
+              <span className="item-count" />
+            </button>
+          </div>
+        </div>
       </div>
       <div className="menuProfilNavbar" onMouseLeave={handleMouseLeave}>
         {isDivVisible && (
-          <>
+          <div>
+            <div>
+              {user !== null && (
+                <>
+                  <div className="imageUser">
+                    <img src={user.image} alt="user" />
+                    <h2>{user.pseudo}</h2>
+                  </div>
+                  <Link to="/profil" className="linkProfilElementNavbar">
+                    <p>Profil</p>
+                  </Link>
+                  <Link
+                    to="/profil/commandesection"
+                    className="linkProfilElementNavbar"
+                  >
+                    <p>Mes commandes</p>
+                  </Link>
+                  <Link
+                    to="/profil/favorissection"
+                    className="linkProfilElementNavbar"
+                  >
+                    <p>Mes favoris</p>
+                  </Link>
+                  <Link
+                    to="/profil/demandeparticuliere"
+                    className="linkProfilElementNavbar"
+                  >
+                    <p>Demandes particulières</p>
+                  </Link>
+                </>
+              )}
+            </div>
             <div className="seConnecter">
               {user === null ? (
                 <button type="button" onClick={handleOpenPopup}>
@@ -74,37 +112,7 @@ function NavBar() {
                 </Link>
               )}
             </div>
-            {user !== null && (
-              <>
-                <div className="imageUser">
-                  <img src={user.image} alt="user" />
-                  <h2>{user.pseudo}</h2>
-                </div>
-
-                <Link to="/profil" className="linkProfilElementNavbar">
-                  Profil
-                </Link>
-                <Link
-                  to="/profil/commandesection"
-                  className="linkProfilElementNavbar"
-                >
-                  Mes commandes
-                </Link>
-                <Link
-                  to="/profil/favorissection"
-                  className="linkProfilElementNavbar"
-                >
-                  Mes favoris
-                </Link>
-                <Link
-                  to="/profil/demandeparticuliere"
-                  className="linkProfilElementNavbar"
-                >
-                  Demandes particulières
-                </Link>
-              </>
-            )}
-          </>
+          </div>
         )}
       </div>
     </div>
