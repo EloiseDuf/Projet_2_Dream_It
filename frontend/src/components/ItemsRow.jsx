b7e4d88c2927700e40e26ace61d1841d87144378
ELOISE DUFAU <eloise.dufau@gmail.com>
2023-06-28 17:22:28 +0200

    panier avec sous total

--- frontend/src/components/ItemsRow.jsx
+++ frontend/src/components/ItemsRow.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import MiniCardsBasket from "./MiniCardsBasket";
 import "./ItemsRow.scss";
 import poubelle from "../assets/images/poubelle.png";
+import MiniCards from "./MiniCards";
 
-function ItemsRow({ dreams }) {
+function ItemsRow({ panierRow }) {
   const [count, setCount] = useState(0);
 
   const handleClickLess = () => {
@@ -16,22 +17,39 @@ function ItemsRow({ dreams }) {
     setCount(count + 1);
   };
 
-  const sousTotal = (count, dreams) => {
-    return count * dreams[0].price;
+  // const handleOnDelete = () => {
+  //   onDelete(panierRow[0]);
+  // };
+
+  const sousTotal = (panierRow) => {
+    return panierRow.reduce((acc, element) => acc + element.price, 0) * count;
   };
 
   return (
     <section className="sectionItemsRow">
-      <div className="cardAndDetails">
-        <div className="div-section-minicards">
-          <MiniCardsBasket dreams={dreams[0]} key={dreams[0].id} />
+      {panierRow.length === 1 ? (
+        <div className="cardAndDetails">
+          <div className="div-section-minicards">
+            <MiniCardsBasket dreams={panierRow[0]} key={panierRow[0].id} />
+          </div>
+          <div className="details">
+            <h1>{panierRow[0].name}</h1>
+            <p>{panierRow[0].price} euros</p>
+            <p className="longText">{panierRow[0].description}</p>
+          </div>
         </div>
-        <div className="details">
-          <h1>{dreams[0].name}</h1>
-          <p>{dreams[0].price} euros</p>
-          <p className="longText">{dreams[0].description}</p>
+      ) : (
+        <div className="cardAndDetails custom">
+          <div className="details">
+            <h1>Le rêve de vos rêves</h1>
+          </div>
+          <div className="div-section-minicards">
+            {panierRow.map((element) => (
+              <MiniCards dreams={element} key={element.id} />
+            ))}
+          </div>
         </div>
-      </div>
+      )}
       <div className="quantity">
         <button className="less" type="button" onClick={handleClickLess}>
           -
@@ -48,10 +66,7 @@ function ItemsRow({ dreams }) {
       </div>
       <div className="total-commande">
         <p>Total</p>
-        <div className="div-commande-price">{`${sousTotal(
-          count,
-          dreams
-        )} €`}</div>
+        <div className="div-commande-price">{`${sousTotal(panierRow)} €`}</div>
       </div>
     </section>
   );
--- frontend/src/pages/Basket.jsx
+++ frontend/src/pages/Basket.jsx
@@ -1,13 +1,23 @@
+import { useContext } from "react";
 import ItemsRow from "../components/ItemsRow";
 import "./Basket.scss";
+import MyContext from "../components/Context";
 
-function Basket({ dreams }) {
+function Basket() {
+  const { user } = useContext(MyContext);
+
+  // destructuration de user
+  const { commandes } = user;
+  const panier = commandes;
+
   return (
     <div className="mainBasketSection">
       <div>
         <p>Mon panier</p>
         <div>
-          <ItemsRow dreams={dreams} />
+          {panier.map((panierRow) => (
+            <ItemsRow panierRow={panierRow} key={panierRow} />
+          ))}
         </div>
       </div>
     </div>
