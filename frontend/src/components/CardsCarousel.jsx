332184566ae59f2d36fb1dd35b98094dd82ad633
Dagets <inard.frederic@gmail.com>
2023-06-29 15:56:21 +0200

    carousel V2bsi

--- frontend/src/components/CardsCarousel.jsx
+++ frontend/src/components/CardsCarousel.jsx
@@ -1,114 +1,148 @@
 // import { useState } from "react";
-import { useState } from "react";
+// import { useState } from "react";
 import Cards from "./Cards";
 import "./CardsCarousel.scss";
 
 function CardsCarousel({ dreams, isOn }) {
-  const calculerNbCardAffichees = () => {
-    const largeurWindow = window.innerWidth;
-    const largeurCards = 300;
-    return Math.floor(largeurWindow / largeurCards);
-  };
-
-  const [cardIndexMin, setCardIndexMin] = useState(0);
-
-  const [cardIndexMax, setCardIndexMax] = useState(calculerNbCardAffichees);
-
-  const [nbCard, setNbCard] = useState(calculerNbCardAffichees);
-
-  const handleClickPreviousCard = () => {
-    if (cardIndexMin > nbCard - 1) {
-      setCardIndexMin(cardIndexMin - nbCard);
-      setCardIndexMax(cardIndexMax - nbCard);
-    }
-  };
-
-  const handleClickNextCard = () => {
-    if (cardIndexMax < dreams.length - (nbCard - 1)) {
-      setCardIndexMin(cardIndexMin + nbCard);
-      setCardIndexMax(cardIndexMax + nbCard);
-    } else {
-      setCardIndexMin(dreams.length - (nbCard + 1));
-      setCardIndexMax(dreams.length + 1);
-    }
-    setCardIndexMin(cardIndexMin + nbCard);
-    setCardIndexMax(cardIndexMax + nbCard);
-  };
-
-  const onResizeNbCard = () => {
-    setNbCard(calculerNbCardAffichees);
-    setCardIndexMax(cardIndexMin + nbCard);
-  };
-  window.onresize = onResizeNbCard;
-
   return (
-    <div className="carouselBox">
-      <div>
-        <button
-          type="button"
-          className="previousButton"
-          onClick={handleClickPreviousCard}
-        >
-          <svg
-            width="50"
-            height="50"
-            viewBox="0 0 16 16"
-            xmlns="http://www.w3.org/2000/svg"
-            style={{ transform: "rotate(90deg)" }}
-          >
-            <defs>
-              <radialGradient id="gradient">
-                <stop offset="50%" stopColor="blue" />
-                <stop offset="60%" stopColor="purple" />
-              </radialGradient>
-            </defs>
-            <path
-              fill="url(#gradient)"
-              d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"
-            />
-          </svg>
-        </button>
+    <>
+      <img
+        className="arrow1"
+        src="https://svgsilh.com/svg/1217938.svg"
+        alt="arrow"
+      />
+      <div className="carouselBox">
+        {isOn
+          ? dreams.map(
+              (dream) =>
+                dream.mode === "dream" && (
+                  <Cards className="cardCarous" dreams={dream} key={dream.id} />
+                )
+            )
+          : dreams.map(
+              (dream) =>
+                dream.mode === "nightmare" && (
+                  <Cards className="cardCarous" dreams={dream} key={dream.id} />
+                )
+            )}
       </div>
-      <div className="mapFlex">
-        {dreams.map((dream) => {
-          if (
-            dream.type === "ready-to-use" &&
-            ((isOn && dream.mode === "dream") ||
-              (!isOn && dream.mode === "nightmare")) &&
-            dream.id > cardIndexMin &&
-            dream.id <= cardIndexMax
-          ) {
-            return <Cards key={dream.id} dreams={dream} />;
-          }
-          return null;
-        })}
-      </div>
-      <button
-        type="button"
-        className="nextButton"
-        onClick={handleClickNextCard}
-      >
-        <svg
-          width="50"
-          height="50"
-          viewBox="0 0 16 16"
-          xmlns="http://www.w3.org/2000/svg"
-          style={{ transform: "rotate(-90deg)" }}
-        >
-          <defs>
-            <radialGradient id="gradient">
-              <stop offset="50%" stopColor="blue" />
-              <stop offset="60%" stopColor="purple" />
-            </radialGradient>
-          </defs>
-          <path
-            fill="url(#gradient)"
-            d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"
-          />
-        </svg>
-      </button>
-    </div>
+      <img
+        className="arrow2"
+        src="https://svgsilh.com/svg/1217938.svg"
+        alt="arrow"
+      />
+    </>
   );
 }
 
 export default CardsCarousel;
+
+// function CardsCarousel({ dreams, isOn }) {
+//   const calculerNbCardAffichees = () => {
+//     const largeurWindow = window.innerWidth;
+//     const largeurCards = 300;
+//     return Math.floor(largeurWindow / largeurCards);
+//   };
+
+//   const [cardIndexMin, setCardIndexMin] = useState(0);
+
+//   const [cardIndexMax, setCardIndexMax] = useState(calculerNbCardAffichees);
+
+//   const [nbCard, setNbCard] = useState(calculerNbCardAffichees);
+
+//   const handleClickPreviousCard = () => {
+//     if (cardIndexMin > nbCard - 1) {
+//       setCardIndexMin(cardIndexMin - nbCard);
+//       setCardIndexMax(cardIndexMax - nbCard);
+//     }
+//   };
+
+//   const handleClickNextCard = () => {
+//     if (cardIndexMax < dreams.length - (nbCard - 1)) {
+//       setCardIndexMin(cardIndexMin + nbCard);
+//       setCardIndexMax(cardIndexMax + nbCard);
+//     } else {
+//       setCardIndexMin(dreams.length - (nbCard + 1));
+//       setCardIndexMax(dreams.length + 1);
+//     }
+//     setCardIndexMin(cardIndexMin + nbCard);
+//     setCardIndexMax(cardIndexMax + nbCard);
+//   };
+
+//   const onResizeNbCard = () => {
+//     setNbCard(calculerNbCardAffichees);
+//     setCardIndexMax(cardIndexMin + nbCard);
+//   };
+//   window.onresize = onResizeNbCard;
+
+//   return (
+//     <div className="carouselBox">
+//       <div>
+//         <button
+//           type="button"
+//           className="previousButton"
+//           onClick={handleClickPreviousCard}
+//         >
+//           <svg
+//             width="50"
+//             height="50"
+//             viewBox="0 0 16 16"
+//             xmlns="http://www.w3.org/2000/svg"
+//             style={{ transform: "rotate(90deg)" }}
+//           >
+//             <defs>
+//               <radialGradient id="gradient">
+//                 <stop offset="50%" stopColor="blue" />
+//                 <stop offset="60%" stopColor="purple" />
+//               </radialGradient>
+//             </defs>
+//             <path
+//               fill="url(#gradient)"
+//               d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"
+//             />
+//           </svg>
+//         </button>
+//       </div>
+//       <div className="mapFlex">
+//         {dreams.map((dream) => {
+//           if (
+//             dream.type === "ready-to-use" &&
+//             ((isOn && dream.mode === "dream") ||
+//               (!isOn && dream.mode === "nightmare")) &&
+//             dream.id > cardIndexMin &&
+//             dream.id <= cardIndexMax
+//           ) {
+//             return <Cards key={dream.id} dreams={dream} />;
+//           }
+//           return null;
+//         })}
+//       </div>
+//       <button
+//         type="button"
+//         className="nextButton"
+//         onClick={handleClickNextCard}
+//       >
+//         <svg
+//           width="50"
+//           height="50"
+//           viewBox="0 0 16 16"
+//           xmlns="http://www.w3.org/2000/svg"
+//           style={{ transform: "rotate(-90deg)" }}
+//         >
+//           <defs>
+//             <radialGradient id="gradient">
+//               <stop offset="50%" stopColor="blue" />
+//               <stop offset="60%" stopColor="purple" />
+//             </radialGradient>
+//           </defs>
+//           <path
+//             fill="url(#gradient)"
+//             d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"
+//           />
+//         </svg>
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default CardsCarousel;
