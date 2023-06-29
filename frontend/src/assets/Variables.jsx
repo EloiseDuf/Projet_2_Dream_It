f6bd343f6af750bd7421c5f694610cddaecef9b6
Jerome Desouches <jerome.desouches@gmail.com>
2023-06-29 16:19:19 +0200

    section commande modifiée pour pouvoir recevoir des commandes avec des paniers composés de plusieurs articles. Envoi des anciennes commandes au panier fonctionnel

--- frontend/src/assets/Variables.jsx
+++ frontend/src/assets/Variables.jsx
@@ -1979,11 +1979,12 @@ const users = [
       "https://th.bing.com/th/id/OIP.qwbSJ0-sbRTlwQRt6lMDNAHaE8?pid=ImgDet&rs=1",
     favoris: [all[1], all[10], all[24], all[48], all[78], all[100]],
     commandes: [
-      [all[10]],
-      [all[24]],
+      [[all[10]], [all[24]]],
       [all[48]],
-      [all[88], all[99], all[102], all[78]],
-      [all[79], all[89], all[99], all[109]],
+      [
+        [all[88], all[99], all[102], all[78]],
+        [all[79], all[89], all[99], all[109]],
+      ],
     ],
   },
   {
@@ -2003,11 +2004,8 @@ const users = [
       "https://image-uviadeo.journaldunet.com/image/450/1755370200/854586.jpg",
     favoris: [all[2], all[11], all[25], all[105]],
     commandes: [
-      [all[11]],
-      [all[26]],
-      [all[58]],
-      [all[89], all[109], all[103], all[79], all[99]],
-      [all[80], all[90], all[100], all[109]],
+      [[all[11]], [all[89], all[109], all[103], all[79], all[99]], [all[26]]],
+      [[all[58]], [all[80], all[90], all[100], all[109]]],
       [all[81], all[91], all[101], all[119]],
     ],
   },
